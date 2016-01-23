e2144178510e4f32f4eb73a4b0e780816c092a92
David Mooney <dmooney@dmooney.org>
2016-01-23 12:01:38 -0500

    Project 1 more tests

--- mc1_p1/analysis.py
+++ mc1_p1/analysis.py
@@ -43,26 +43,11 @@ def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
         df_temp = pd.concat([port_val_normed, prices_SPY_normed], keys=['Portfolio', 'SPY'], axis=1)
         plot_data(df_temp)
 
-    # TODO Add code here to properly compute end value
-    ev = sv
+    ev = port_val[-1]
 
     return cr, adr, sddr, sr, ev
 
-if __name__ == "__main__":
-    # This code WILL NOT be tested by the auto grader
-    # It is only here to help you set up and test your code
-
-    # Define input parameters
-    # Note that ALL of these values will be set to different values by
-    # the autograder!
-    start_date = dt.datetime(2010,1,1)
-    end_date = dt.datetime(2010,12,31)
-    symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
-    allocations = [0.2, 0.3, 0.4, 0.1]
-    start_val = 1000000  
-    risk_free_rate = 0.0
-    sample_freq = 252
-
+def test(sd, ed, syms, allocs, sv, rfr, sf):
     # Assess the portfolio
     cr, adr, sddr, sr, ev = assess_portfolio(sd = start_date, ed = end_date,\
         syms = symbols, \
@@ -79,3 +64,30 @@ if __name__ == "__main__":
     print "Volatility (stdev of daily returns):", sddr
     print "Average Daily Return:", adr
     print "Cumulative Return:", cr
+    print "End Val", ev
+
+if __name__ == "__main__":
+    # This code WILL NOT be tested by the auto grader
+    # It is only here to help you set up and test your code
+
+    # Define input parameters
+    # Note that ALL of these values will be set to different values by
+    # the autograder!
+    start_date = dt.datetime(2010,1,1)
+    end_date = dt.datetime(2010,12,31)
+    symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
+    allocations = [0.2, 0.3, 0.4, 0.1]
+    start_val = 1000000  
+    risk_free_rate = 0.0
+    sample_freq = 252
+
+    test(start_date, end_date, symbols, allocations, start_val, risk_free_rate, sample_freq)
+
+    symbols = ['AXP', 'HPQ', 'IBM', 'HNZ']
+    allocations = [0.0, 0.0, 0.0, 1.0]
+    test(start_date, end_date, symbols, allocations, start_val, risk_free_rate, sample_freq)
+
+    start_date = dt.datetime(2010,6,1)
+    symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
+    allocations = [0.2, 0.3, 0.4, 0.1]
+    test(start_date, end_date, symbols, allocations, start_val, risk_free_rate, sample_freq)
