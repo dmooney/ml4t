b3a5ececb3b10bc7feecafe39c425775441734e7
David Mooney <dmooney@dmooney.org>
2016-02-15 22:54:47 -0500

    MC2-P1 Everything working except stats for leverage2 and 3

--- mc2_p1/marketsim.py
+++ mc2_p1/marketsim.py
@@ -6,14 +6,78 @@ import datetime as dt
 import os
 from util import get_data, plot_data
 
+def execute_order(cash, holdings, order, prices):
+    date = dt.datetime.strptime(order['Date'], '%Y-%m-%d')
+    symbol = order['Symbol']
+    order_type = order['Order']
+    shares = order['Shares']
+
+    new_holdings = holdings.copy()
+
+    if date in prices.index:
+        price = prices[[symbol]].loc[date][0]
+        if order_type == 'BUY':
+            new_cash = cash - (price * shares)
+            new_holdings[symbol] = holdings[symbol] + shares
+        else:
+            new_cash = cash + (price * shares)
+            new_holdings[symbol] = holdings[symbol] - shares
+
+    longs = 0
+    shorts = 0
+    for holding in new_holdings:
+        price = prices[[holding]].loc[date][0]
+        if (new_holdings[holding] > 0):
+            longs = longs + price * new_holdings[holding]
+        elif (new_holdings[holding] < 0):
+            shorts = shorts + price * new_holdings[holding]
+
+    leverage = (longs + abs(shorts)) / (longs + shorts + new_cash)
+    if leverage > 2.0:
+        print("rejected: ", date, symbol, order_type, shares, holdings, cash, leverage, longs, shorts)
+        return cash, holdings
+    else:
+        print("executed: ", date, symbol, order_type, shares, new_holdings, new_cash, leverage, longs, shorts)
+        return new_cash, new_holdings
+
+def print_stats(start_date, end_date, prices, portvals, sf=252.0, rfr=0.0):
+
+    cum_ret = (portvals["Value"].iget(-1)/portvals["Value"].iget(0)) - 1
+
+
+    dr = portvals.copy()
+    dr[1:] = portvals[1:].values / portvals[:-1].values - 1
+    dr = dr[1:]
+    avg_daily_ret = dr.mean()
+    std_daily_ret = dr.std()
+    sharpe_ratio = np.sqrt(sf) * np.mean(dr - rfr) / std_daily_ret
+    ev = portvals["Value"].iget(-1)
+
+    print "Date Range: {} to {}".format(start_date, end_date)
+    print
+    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
+    # print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)
+    print
+    print "Cumulative Return of Fund: {}".format(cum_ret)
+    # print "Cumulative Return of SPY : {}".format(cum_ret_SPY)
+    print
+    print "Standard Deviation of Fund: {}".format(std_daily_ret)
+    # print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)
+    print
+    print "Average Daily Return of Fund: {}".format(avg_daily_ret)
+    # print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
+    print
+    print "Final Portfolio Value: {}".format(portvals["Value"].iget(-1))
+
+
+
+
 def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
     # this is the function the autograder will call to test your code
-    # TODO: Your code here
     orders = pd.read_csv(orders_file, parse_dates=True)
-    # print(orders)
 
-    start_date = orders['Date'][0]
-    end_date = orders['Date'][len(orders)-1]
+    start_date = orders['Date'].iloc[0]
+    end_date = orders['Date'].iloc[len(orders)-1]
     symbols = sorted(set(orders['Symbol']))
     prices = get_data(symbols, pd.date_range(start_date, end_date))
 
@@ -22,31 +86,25 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
     for symbol in symbols:
         holdings[symbol] = 0
 
-    for row in orders.iterrows():
-        order = row[1]
-        date = dt.datetime.strptime(order['Date'], '%Y-%m-%d')
-        symbol = order['Symbol']
-        order_type = order['Order']
-        shares = order['Shares']
-        if date in prices.index:
-            price = prices[[symbol]].loc[date][0]
-            if order_type == 'BUY':
-                cash = cash - (price * shares)
-                holdings[symbol] = holdings[symbol] + shares
-            else:
-                cash = cash + (price * shares)
-                holdings[symbol] = holdings[symbol] - shares
-        else:
-            print("Rejecting trade on ", date)
-        print(order, cash, holdings)
+    portvals = pd.DataFrame(index=prices.index, columns=['Value'])
 
+    for index in portvals.index:
 
-    # In the template, instead of computing the value of the portfolio, we just
-    # read in the value of IBM over 6 months
-    start_date = dt.datetime(2008,1,1)
-    end_date = dt.datetime(2008,6,1)
-    portvals = get_data(['IBM'], pd.date_range(start_date, end_date))
-    portvals = portvals[['IBM']]  # remove SPY
+        # If there are orders on this date, execute them
+        date_orders = orders[orders['Date'] == str(index.date())]
+        for row in date_orders.iterrows():
+            order = row[1]
+            cash, holdings = execute_order(cash, holdings, order, prices)
+
+        # Calculate port val for this date
+        portvals.ix[index] = cash
+        for symbol in holdings:
+            if (holdings[symbol] != 0):
+                price = prices[[symbol]].loc[index.date()][0]
+                portvals.ix[index] = portvals.ix[index] + (price * holdings[symbol])
+        print(portvals.ix[index])
+
+    print_stats(start_date, end_date, prices, portvals)
 
     return portvals
 
@@ -55,39 +113,43 @@ def test_code():
     # note that during autograding his function will not be called.
     # Define input parameters
 
-    of = "./orders/orders2.csv"
+    of = "./orders/orders-leverage-2.csv"
+    print(of)
     sv = 1000000
 
     # Process orders
     portvals = compute_portvals(orders_file = of, start_val = sv)
+    # plot_data(portvals)
     if isinstance(portvals, pd.DataFrame):
         portvals = portvals[portvals.columns[0]] # just get the first column
     else:
         "warning, code did not return a DataFrame"
-    
-    # Get portfolio stats
-    # Here we just fake the data. you should use your code from previous assignments.
-    start_date = dt.datetime(2008,1,1)
-    end_date = dt.datetime(2008,6,1)
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [0.2,0.01,0.02,1.5]
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]
 
-    # Compare portfolio against $SPX
-    print "Date Range: {} to {}".format(start_date, end_date)
-    print
-    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
-    print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)
-    print
-    print "Cumulative Return of Fund: {}".format(cum_ret)
-    print "Cumulative Return of SPY : {}".format(cum_ret_SPY)
-    print
-    print "Standard Deviation of Fund: {}".format(std_daily_ret)
-    print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)
-    print
-    print "Average Daily Return of Fund: {}".format(avg_daily_ret)
-    print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
-    print
-    print "Final Portfolio Value: {}".format(portvals[-1])
+    # print(portvals)
+    
+    # # Get portfolio stats
+    # # Here we just fake the data. you should use your code from previous assignments.
+    # start_date = dt.datetime(2008,1,1)
+    # end_date = dt.datetime(2008,6,1)
+    # cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [0.2,0.01,0.02,1.5]
+    # cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]
+    #
+    # # Compare portfolio against $SPX
+    # print "Date Range: {} to {}".format(start_date, end_date)
+    # print
+    # print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
+    # print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)
+    # print
+    # print "Cumulative Return of Fund: {}".format(cum_ret)
+    # print "Cumulative Return of SPY : {}".format(cum_ret_SPY)
+    # print
+    # print "Standard Deviation of Fund: {}".format(std_daily_ret)
+    # print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)
+    # print
+    # print "Average Daily Return of Fund: {}".format(avg_daily_ret)
+    # print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
+    # print
+    # print "Final Portfolio Value: {}".format(portvals[-1])
 
 if __name__ == "__main__":
     test_code()
