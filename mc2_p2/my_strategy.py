859f7eb2ed854531214eb1d4cd4e6b09e1e51697
David Mooney <dmooney@dmooney.org>
2016-03-21 22:16:06 -0400

    MC2-P2 Initial commit

---
+++ mc2_p2/my_strategy.py
@@ -0,0 +1,10 @@
+# author: David Mooney (dmooney3)
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+import os
+from util import get_data, plot_data
+
+if __name__ == "__main__":
+    pass
