8e7326541ef54778bb75165efd838a60b4772a31
David Mooney <dmooney@dmooney.org>
2016-04-18 01:22:59 -0400

    MC3 P2 Good in sample strat plots 1, 2, 3, 6, 7 done

--- mc3_p2/mc3_p2.py
+++ mc3_p2/mc3_p2.py
@@ -10,6 +10,112 @@ import LinRegLearner as lrl
 import KNNLearner as knn
 import BagLearner as bl
 
+
+def execute_order(cash, holdings, order, prices):
+    date = dt.datetime.strptime(order['Date'], '%Y-%m-%d')
+    symbol = order['Symbol']
+    order_type = order['Order']
+    shares = order['Shares']
+
+    new_holdings = holdings.copy()
+
+    if date in prices.index:
+        price = prices[[symbol]].loc[date][0]
+        if order_type == 'BUY':
+            new_cash = cash - (price * shares)
+            new_holdings[symbol] = holdings[symbol] + shares
+        else:
+            new_cash = cash + (price * shares)
+            new_holdings[symbol] = holdings[symbol] - shares
+
+    longs = 0
+    shorts = 0
+    for holding in new_holdings:
+        price = prices[[holding]].loc[date][0]
+        if (new_holdings[holding] > 0):
+            longs = longs + price * new_holdings[holding]
+        elif (new_holdings[holding] < 0):
+            shorts = shorts + price * new_holdings[holding]
+
+    leverage = (longs + abs(shorts)) / (longs + shorts + new_cash)
+    # if leverage > 2.0:
+    #     # print("rejected: ", date, symbol, order_type, shares, holdings, cash, leverage, longs, shorts)
+    #     return cash, holdings
+    # else:
+        # print("executed: ", date, symbol, order_type, shares, new_holdings, new_cash, leverage, longs, shorts)
+    return new_cash, new_holdings
+
+def print_stats(start_date, end_date, prices, portvals, sf=252.0, rfr=0.0, baseline_perf=0.3524):
+
+    cum_ret = portvals["Value"].iget(-1)/portvals["Value"].iget(0) - 1
+
+    dr = portvals.copy()
+    dr[1:] = portvals[1:].values / portvals[:-1].values - 1.0
+    dr = dr[1:]
+    avg_daily_ret = dr.mean()
+    std_daily_ret = dr.std()
+    sharpe_ratio = np.sqrt(sf) * np.mean(dr - rfr) / std_daily_ret
+    ev = portvals["Value"].iget(-1)
+
+    print "Date Range: {} to {}".format(start_date, end_date)
+    print
+    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
+    # print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)
+    print
+    print "Cumulative Return of Fund: {}".format(cum_ret)
+    # print "Cumulative Return of SPY : {}".format(cum_ret_SPY)
+    print
+    print "Standard Deviation of Fund: {}".format(std_daily_ret)
+    # print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)
+    print
+    print "Average Daily Return of Fund: {}".format(avg_daily_ret)
+    # print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
+    print
+    print "Final Portfolio Value: {}".format(portvals["Value"].iget(-1))
+
+    # print
+    # print "Performance vs baseline: {}".format(cum_ret / baseline_perf)
+    # return cum_ret/baseline_perf
+
+
+
+def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000, baseline_perf=0.3524):
+    # this is the function the autograder will call to test your code
+    orders = pd.read_csv(orders_file, parse_dates=True)
+
+    start_date = orders['Date'].iloc[0]
+    end_date = orders['Date'].iloc[len(orders)-1]
+    symbols = sorted(set(orders['Symbol']))
+    prices = get_data(symbols, pd.date_range(start_date, end_date))
+
+    cash = start_val
+    holdings = {}
+    for symbol in symbols:
+        holdings[symbol] = 0
+
+    portvals = pd.DataFrame(index=prices.index, columns=['Value'], dtype=np.float64)
+    # print(type(portvals))
+
+    for index in portvals.index:
+
+        # If there are orders on this date, execute them
+        date_orders = orders[orders['Date'] == str(index.date())]
+        for row in date_orders.iterrows():
+            order = row[1]
+            cash, holdings = execute_order(cash, holdings, order, prices)
+
+        # Calculate port val for this date
+        portvals.ix[index] = cash
+        for symbol in holdings:
+            if (holdings[symbol] != 0):
+                price = prices[[symbol]].loc[index.date()][0]
+                portvals.ix[index] = portvals.ix[index] + (price * holdings[symbol])
+        # print(portvals.ix[index])
+
+    performance = print_stats(start_date, end_date, prices, portvals, baseline_perf=baseline_perf)
+
+    return portvals, performance
+
 def add_bb(prices, symbol):
     sma = pd.rolling_mean(prices[symbol], 20, 20)
     prices['bb_normed'] = (prices[symbol] - sma)/(2 * pd.rolling_std(prices[symbol], 20, 20))
@@ -86,10 +192,10 @@ if __name__ == "__main__":
     # test_rows = data.shape[0] - train_rows
 
     # separate out training and testing data
-    trainX = X['20071231':'20091231'][75:]
-    trainY = Y['20071231':'20091231'][75:]
-    testX = X['20091231':'20111231'][75:]
-    testY = Y['20091231':'20111231'][75:]
+    trainX = X['20071231':'20091231']
+    trainY = Y['20071231':'20091231']
+    testX = X['20091231':'20111231']
+    testY = Y['20091231':'20111231']
 
     # print testX.shape
     # print testY.shape
@@ -155,3 +261,91 @@ if __name__ == "__main__":
     # chart.set_ylabel("Price")
     # fig = chart.get_figure()
     # fig.savefig(symbol + "_plot1_oos.png")
+
+    order_chart = prices.plot(title=symbol)
+    order_chart.set_xlabel("Date")
+    order_chart.set_ylabel("Price")
+
+    order_cols = ("Date", "Symbol", "Order", "Shares")
+    orders = pd.DataFrame(columns=order_cols)
+
+    state = 'cash'
+
+    hold = 0
+    for i in xrange(predY.shape[0]):
+        today = prices.index[i]
+        # print(i, today, predY[i])
+
+        if hold > 0:
+            hold = hold - 1
+            continue
+
+        if state == 'cash':
+            if predY[i] > 0.01:
+                state = 'long'
+                chart.axvline(today, color="g")
+                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                hold = 5
+            elif predY[i] < -0.01:
+                state = 'short'
+                chart.axvline(today, color="r")
+                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                hold = 5
+
+        elif state == 'short':
+            if predY[i] > 0 and predY[i] < 0.01:
+                state = 'cash'
+                chart.axvline(today, color="k")
+                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                hold = 5
+            elif predY[i] > 0.01:
+                state = 'long'
+                chart.axvline(today, color="g")
+                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                hold = 5
+
+        else: #long
+            if predY[i] < 0.01 and predY[i] > 0:
+                state = 'cash'
+                chart.axvline(today, color="k")
+                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                hold = 5
+            elif predY[i] < -0.01:
+                state = 'short'
+                chart.axvline(today, color="r")
+                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                hold = 5
+
+
+    if state == 'long':
+        orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+        chart.axvline(today, color="k")
+    elif state == 'short':
+        orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+        chart.axvline(today, color="k")
+
+
+    fig = chart.get_figure()
+    fig.savefig(symbol + '_plot2.png')
+    # plt.show()
+
+    baseline_perf=0.3524
+    if len(orders) > 1:
+        orders.to_csv('orders.csv', index=False)
+        portvals, performance = compute_portvals('orders.csv', start_val=10000, baseline_perf=baseline_perf)
+    else:
+        performance = 0.0
+
+    print(performance)
+
+    start_date = orders['Date'].iloc[0]
+    end_date = orders['Date'].iloc[len(orders)-1]
+    prices_SPX = get_data(['$SPX'], pd.date_range(start_date, end_date), addSPY=False)
+    prices_SPX.dropna(inplace=True)
+
+    port_val_normed = portvals / portvals.ix[0]
+    prices_SPX_normed = prices_SPX / prices_SPX.ix[0]
+    df_temp = pd.concat([port_val_normed, prices_SPX_normed], keys=['Portfolio_' + symbol, '$SPX'], axis=1)
+    plot_data(df_temp)
