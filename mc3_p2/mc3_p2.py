bb512eeb1b6a788560a84dbb2ed779b2bd34c961
David Mooney <dmooney@dmooney.org>
2016-04-18 19:43:39 -0400

    MC3 P2 Refactored code for submission

--- mc3_p2/mc3_p2.py
+++ mc3_p2/mc3_p2.py
@@ -122,11 +122,7 @@ def add_bb(prices, symbol):
     # prices['Lower_normed'] = prices[symbol] + prices['SMA']/-2 * pd.rolling_std(prices[symbol], 20, 20)
     return prices
 
-if __name__ == "__main__":
-    symbol = 'IBM'
-    # symbol = 'ML4T-220'
-
-
+def mc3p2(symbol):
     prices = get_data([symbol], pd.date_range(dt.datetime(2007,12,31), dt.datetime(2011,12,31)), False)
     prices.dropna(inplace=True)
     prices = add_bb(prices, symbol)
@@ -192,10 +188,15 @@ if __name__ == "__main__":
     # test_rows = data.shape[0] - train_rows
 
     # separate out training and testing data
-    trainX = X['20071231':'20091231']
-    trainY = Y['20071231':'20091231']
-    testX = X['20091231':'20111231']
-    testY = Y['20091231':'20111231']
+
+    train_start = '20071231'
+    train_end = '20091231'
+    test_start = '20091231'
+    test_end = '20111231'
+    trainX = X[train_start:train_end]
+    trainY = Y[train_start:train_end]
+    testX = X[test_start:test_end]
+    testY = Y[test_start:test_end]
 
     # print testX.shape
     # print testY.shape
@@ -215,29 +216,30 @@ if __name__ == "__main__":
     learner.addEvidence(trainX, trainY) # train it
 
     # evaluate in sample
-    # predY = learner.query(trainX) # get the predictions
-    # rmse = np.sqrt(((trainY - predY) ** 2).sum()/trainY.shape[0])
-    # print
-    # print "In sample results"
-    # print "RMSE: ", rmse
-    # c = np.corrcoef(predY, y=trainY)
-    # print "corr: ", c[0,1]
-    #
-    # plot1 = prices.copy()
-    # del plot1['bb_normed']
-    # del plot1['15div75sma']
-    # del plot1[symbol + '_normed']
-    # del plot1['momentum']
-    # plot1["Training Y"] = ((1.0 + Y)  *  prices[symbol])
-    # plot1["Predicted Y"] = ((1.0 + predY) *  prices[symbol])
-    # # print(plot1['20071231':'20091231'].tail())
-    # chart = plot1['20071231':'20091231'].plot(title=symbol)
-    # chart.set_xlabel("Date")
-    # chart.set_ylabel("Price")
-    # fig = chart.get_figure()
-    # fig.savefig(symbol + "_plot1.png")
+    predY = learner.query(trainX) # get the predictions
+    rmse = np.sqrt(((trainY - predY) ** 2).sum()/trainY.shape[0])
+    print
+    print "In sample results"
+    print "RMSE: ", rmse
+    c = np.corrcoef(predY, y=trainY)
+    print "corr: ", c[0,1]
+
+    plot1 = prices.copy()
+    del plot1['bb_normed']
+    del plot1['15div75sma']
+    del plot1[symbol + '_normed']
+    del plot1['momentum']
+    plot1["Training Y"] = ((1.0 + Y)  *  prices[symbol])
+    plot1["Predicted Y"] = ((1.0 + predY) *  prices[symbol])
+    # print(plot1['20071231':'20091231'].tail())
+    chart = plot1[train_start:train_end].plot(title=symbol + " in sample prediction")
+    chart.set_xlabel("Date")
+    chart.set_ylabel("Price")
+    fig = chart.get_figure()
+    fig.savefig(symbol + "_in_plot1.png")
     # plt.show()
 
+    sim(predY, prices, train_start, train_end, symbol, "in sample")
 
     # evaluate out of sample
     predY = learner.query(testX) # get the predictions
@@ -248,35 +250,21 @@ if __name__ == "__main__":
     c = np.corrcoef(predY, y=testY)
     print "corr: ", c[0,1]
 
-    plot1_oos = prices.copy()
-    del plot1_oos['bb_normed']
-    del plot1_oos['15div75sma']
-    del plot1_oos[symbol + '_normed']
-    del plot1_oos['momentum']
-    plot1_oos["Training Y"] = ((1.0 + Y)  *  prices[symbol])
-    plot1_oos["Predicted Y"] = ((1.0 + predY) *  prices[symbol])
-    # print(plot1_oos['20071231':'20091231'].tail())
-    chart = plot1_oos['20091231':'20111231'].plot(title=symbol)
-    chart.set_xlabel("Date")
-    chart.set_ylabel("Price")
-    fig = chart.get_figure()
-    fig.savefig(symbol + "_plot1_oos.png")
+    sim(predY, prices, test_start, test_end, symbol, "out of sample")
 
-    orders_chart_prices = prices['20091231':'20111231'].copy()
+
+def sim(predY, prices, start, end, symbol, sample_str):
+    orders_chart_prices = prices[start:end].copy()
     del orders_chart_prices['bb_normed']
     del orders_chart_prices['15div75sma']
     del orders_chart_prices[symbol + '_normed']
     del orders_chart_prices['momentum']
-
-    order_chart = orders_chart_prices.plot(title=symbol)
+    order_chart = orders_chart_prices.plot(title=symbol + " " + sample_str + " entry/exit")
     order_chart.set_xlabel("Date")
     order_chart.set_ylabel("Price")
-
     order_cols = ("Date", "Symbol", "Order", "Shares")
     orders = pd.DataFrame(columns=order_cols)
-
     state = 'cash'
-
     hold = 0
     for i in xrange(predY.shape[0]):
         today = orders_chart_prices.index[i]
@@ -290,68 +278,67 @@ if __name__ == "__main__":
             if predY[i] > 0.01:
                 state = 'long'
                 order_chart.axvline(today, color="g")
-                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                orders.loc[len(orders)] = [today, symbol, "BUY", 100]
                 hold = 5
             elif predY[i] < -0.01:
                 state = 'short'
                 order_chart.axvline(today, color="r")
-                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                orders.loc[len(orders)] = [today, symbol, "SELL", 100]
                 hold = 5
 
         elif state == 'short':
             if predY[i] > 0 and predY[i] < 0.01:
                 state = 'cash'
                 order_chart.axvline(today, color="k")
-                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                orders.loc[len(orders)] = [today, symbol, "BUY", 100]
                 hold = 5
             elif predY[i] > 0.01:
                 state = 'long'
                 order_chart.axvline(today, color="g")
-                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
-                orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+                orders.loc[len(orders)] = [today, symbol, "BUY", 100]
+                orders.loc[len(orders)] = [today, symbol, "BUY", 100]
                 hold = 5
 
-        else: #long
+        else:  # long
             if predY[i] < 0.01 and predY[i] > 0:
                 state = 'cash'
                 order_chart.axvline(today, color="k")
-                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                orders.loc[len(orders)] = [today, symbol, "SELL", 100]
                 hold = 5
             elif predY[i] < -0.01:
                 state = 'short'
                 order_chart.axvline(today, color="r")
-                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
-                orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+                orders.loc[len(orders)] = [today, symbol, "SELL", 100]
+                orders.loc[len(orders)] = [today, symbol, "SELL", 100]
                 hold = 5
-
-
     if state == 'long':
-        orders.loc[len(orders)]=[today, symbol, "SELL", 100]
+        orders.loc[len(orders)] = [today, symbol, "SELL", 100]
         order_chart.axvline(today, color="k")
     elif state == 'short':
-        orders.loc[len(orders)]=[today, symbol, "BUY", 100]
+        orders.loc[len(orders)] = [today, symbol, "BUY", 100]
         order_chart.axvline(today, color="k")
-
-
     fig = order_chart.get_figure()
-    fig.savefig(symbol + '_oos_entry_exit.png')
+    fig.savefig(symbol + '_' + sample_str + '_entry_exit.png')
     # plt.show()
-
-    baseline_perf=0.3524
+    baseline_perf = 0.3524
     if len(orders) > 1:
         orders.to_csv('orders.csv', index=False)
         portvals, performance = compute_portvals('orders.csv', start_val=10000, baseline_perf=baseline_perf)
     else:
         performance = 0.0
-
     print(performance)
-
     start_date = orders['Date'].iloc[0]
-    end_date = orders['Date'].iloc[len(orders)-1]
+    end_date = orders['Date'].iloc[len(orders) - 1]
     prices_SPX = get_data(['$SPX'], pd.date_range(start_date, end_date), addSPY=False)
     prices_SPX.dropna(inplace=True)
-
     port_val_normed = portvals / portvals.ix[0]
     prices_SPX_normed = prices_SPX / prices_SPX.ix[0]
     df_temp = pd.concat([port_val_normed, prices_SPX_normed], keys=['Portfolio_' + symbol, '$SPX'], axis=1)
-    plot_data(df_temp)
+    plot_data(df_temp, title=symbol + " " + sample_str + " backtest")
+
+
+if __name__ == "__main__":
+    symbols = ['IBM', 'ML4T-220']
+
+    for symbol in symbols:
+        mc3p2(symbol)
